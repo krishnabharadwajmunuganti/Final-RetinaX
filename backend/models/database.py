ae47bed8b0edb86f4fe8cdb303fe59a0cbb3f42a
krishnabharadwajmunuganti <krishnabharadwajmunuganti@gmail.com>
2026-09-10 11:52:55 +0530

    conected website

--- backend/models/database.py
+++ backend/models/database.py
@@ -11,7 +11,7 @@ connect_args = {}
 if database_url.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(database_url, connect_args=connect_args)
+engine = create_engine(database_url, connect_args=connect_args, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
